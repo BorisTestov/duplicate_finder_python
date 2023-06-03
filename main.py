5d3cc87ae5ad57ce27d826dcec7eecf6fb1a9cc2
BorisTestov <boris.testov@gmail.com>
2023-06-03 05:39:52 +0300

    Fix build

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-# from version import APP_VERSION, BUILD_NUMBER
-
 import sys
 
 from PySide6.QtWidgets import QApplication
 
 from ui.mainwindow import MainWindow
+from version import APP_VERSION, BUILD_NUMBER
 
 if __name__ == "__main__":
+    print(f"version: {APP_VERSION} build: {BUILD_NUMBER}")
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
